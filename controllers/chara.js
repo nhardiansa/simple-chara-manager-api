8ba7ae744208bbe758b9f891e05515b540229bea
nhardiansa <nhardiansa@gmail.com>
2021-08-30 22:27:48 +0800

    telah membuat ep getById, update, delete

--- controllers/chara.js
+++ controllers/chara.js
@@ -15,7 +15,23 @@ const getAllChara = async (req, res) => {
     })
     console.error(err)
   }
-  const result = await charaModel.findAll(); 
+}
+
+const getCharaById = async (req, res) => {
+  try {
+    const charaId = req.params.id
+    const result = await charaModel.findOne({ where: {id: charaId } })
+    res.json({
+      status: 'success',
+      data: result
+    })
+  } catch (err) {
+    res.json({
+      status: 'error',
+      messages: err.message
+    })
+    console.error(err)
+  }
 }
 
 const addChara = (req, res) => {
@@ -37,4 +53,43 @@ const addChara = (req, res) => {
     })
 }
 
-module.exports = {getAllChara, addChara}
+const updateChara = (req, res) => {
+  const charaId = req.params.id
+  const {name, email} = req.body
+  charaModel.update({name, email}, { where: { id: charaId}})
+    .then(result => {
+      res.status(200)
+      res.json({
+        status: 'success update chara',
+        data: result
+      })
+    })
+    .catch(err => {
+      res.status(400)
+      res.json({
+        status: 'error',
+        message: err.message
+      })
+    })
+}
+
+const deleteCharaById = (req, res) => {
+  const id = req.params.id
+  charaModel.destroy({ where: {id} })
+    .then(result => {
+      res.status(200)
+      res.json({
+        status: 'success delete chara',
+        data: result
+      })
+    })
+    .catch(err => {
+      res.status(404)
+      res.json({
+        status: 'error',
+        message: err.message
+      })
+    })
+}
+
+module.exports = {getAllChara, getCharaById, addChara, updateChara, deleteCharaById}
